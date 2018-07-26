3cc66c110792f8b6127e40e39d44ee19a27383cf
Francisco Godinho <f.miguelgodinho@gmail.com>
2018-07-26 01:54:56 +0100

    obtaining smart contracts and displaying them on the screen

--- app/src/main/java/unl/fct/fgodinho/dslapp/ContractActivity.java
+++ app/src/main/java/unl/fct/fgodinho/dslapp/ContractActivity.java
@@ -1,7 +1,11 @@
 package unl.fct.fgodinho.dslapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import unl.fct.fgodinho.dslapp.network.ApiHttpsRequest;
 import unl.fct.fgodinho.dslapp.network.ApiHttpsRequestResult;
@@ -29,7 +33,7 @@ public class ContractActivity extends BaseActivity {
     protected void onStart() {
         super.onStart();
 
-        String invocationUrl = smartHubUrl + "/" + channelName + "/contract/" + contractId;
+        String invocationUrl = smartHubUrl + "/api/" + channelName + "/contract/" + contractId;
 
         // fire an http request to the sh
         new ApiHttpsRequest(new ApiHttpsRequest.ApiHttpsRequestDelegate() {
@@ -37,6 +41,29 @@ public class ContractActivity extends BaseActivity {
             public void processFinish(ApiHttpsRequestResult output, Exception ex) {
                 if (ex != null) {
                     Toast.makeText(getApplicationContext(), "Error performing HTTPS request to " + invocationUrl + ": " + ex.toString(), Toast.LENGTH_LONG).show();
+                } else {
+
+                    try {
+
+                        // parse contract
+                        JSONObject contractRootJson = new JSONObject(output.getContent());
+                        JSONObject contractJson = new JSONObject(contractRootJson.getString("contract"));
+                        String contractHash = contractRootJson.getString("hash");
+                        boolean contractIsSigned = contractRootJson.getBoolean("is-signed");
+
+                        // display contract on view
+                        TextView contractView = findViewById(R.id.contract_view);
+                        TextView contractHashView = findViewById(R.id.contract_hash);
+                        contractView.setText(contractJson.toString(2));
+                        contractHashView.setText(contractHash);
+
+                        if (contractIsSigned) {
+
+                        }
+
+                    } catch (JSONException je) {
+                        Toast.makeText(getApplicationContext(), "Error interpreting JSON response: " + je.toString(), Toast.LENGTH_LONG).show();
+                    }
                 }
 
             }
